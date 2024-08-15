2e2bd07bda077c49eaa93d1ad9aff94f72f82c65
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-08-15 16:49:37 +0300

    initial commit

---
+++ script/mainApp.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let tWabApp = window.Telegram.WebApp
+
+    const MAX_VALUE = 1000000000000000000
+
+    const infoStart = document.getElementById("info-text-start");
+    const infoLoad = document.getElementById("info-text-load");
+    const loader = document.getElementById("loader");
+    const qrLink = document.getElementById("qrLink");
+
+    init();
+
+    function init() {
+        tWabApp.MainButton.setText("Scan QR code");
+        tWabApp.onEvent('mainButtonClicked', showQRScanner);
+        tWabApp.onEvent('qrTextReceived', processQRCode);
+        tWabApp.MainButton.show()
+        tWabApp.MainButton.enable()
+    }
+
+    function showQRScanner() {
+        let par = {
+            text: ""
+        };
+        tWabApp.showScanQrPopup(par)
+    }
+
+    function zeroPad(nr, base) {
+        var len = (String(base).length - String(nr).length) + 1;
+        return len > 0 ? new Array(len).join('0') + nr : nr;
+    }
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    function openLink(url) {
+        tWabApp.openLink(url, try_instant_view = true);
+    }
+
+    function processQRCode(data) {
+        idNumber = getRandomInt(MAX_VALUE)
+        idText = zeroPad(idNumber, MAX_VALUE)
+        const suffix = "?uuid=20306&amount=7.00&trxid=" + idText + "&type=05"
+        let url = "https://www.google.com"
+
+        qrLink.innerHTML = "<a id=\"qrLink\">Оплатить</a>"
+        qrLink.href = `javascript:Telegram.WebApp.openLink('${url}')`
+
+        tWabApp.MainButton.hide()
+
+        tWabApp.closeScanQrPopup()
+
+        openLink(url)
+
+        infoLoad.style.display = "block"
+        loader.style.display = "none"
+        infoStart.style.display = "none"
+    }
+});
